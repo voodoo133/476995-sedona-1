6d796db4b47a32665599b8deb36e556fa509df40
Anton <voodoo_133@mail.ru>
2018-10-26 14:39:14 +0300

    Правки по ТЗ

--- source/js/script.js
+++ source/js/script.js
@@ -2,18 +2,14 @@ var body = document.body;
 
 if (body.classList.contains('no-js')) body.classList.remove('no-js');
 
-var button_menu_open = document.querySelector('.page-header__open-menu');
-var button_menu_close = document.querySelector('.main-nav__close-menu');
+var control_menu = document.querySelector('.page-header__control-menu');
 var menu = document.querySelector('.main-nav');
 
-button_menu_open.onclick = function (e) {
+control_menu.onclick = function (e) {
   e.preventDefault();
-  menu.classList.add('main-nav--opened');
-}
-
-button_menu_close.onclick = function (e) {
-  e.preventDefault();
-  menu.classList.remove('main-nav--opened');
+  
+  control_menu.classList.toggle('page-header__control-menu--close');
+  menu.classList.toggle('main-nav--opened');
 }
 
 svg4everybody();
